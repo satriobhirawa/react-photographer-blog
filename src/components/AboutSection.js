373799d24f91fb117649ff3d4434aa55f22708d4
satriobhirawa <satrio.bhirawa@yahoo.com>
2021-12-12 16:53:25 +0100

    add styled component to about and service section, add styles.js

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,6 +1,7 @@
 import React from "react";
 import home1 from "../img/home1.png";
 import styled from "styled-components";
+import { About, Description, Image, Hide } from "../styles";
 
 const AboutSection = () => {
   return (
@@ -18,7 +19,7 @@ const AboutSection = () => {
           <Hide>
             <h2>true</h2>
           </Hide>
-          <p>Contact us for make your creativity into reality</p>
+          <p>Contact us to make your creativity into reality</p>
 
           <button>Contact us</button>
         </div>
@@ -29,36 +30,5 @@ const AboutSection = () => {
     </About>
   );
 };
-
-const About = styled.div`
-  min-height: 90vh;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 5rem 10rem;
-  color: white;
-`;
-
-const Description = styled.div`
-  flex: 1;
-  padding-right: 5rem;
-  h2 {
-    font-weight: lighter;
-  }
-`;
-
-const Image = styled.div`
-  flex: 1;
-  overflow: hidden;
-  img {
-    width: 100%auto;
-    height: 80vh;
-    object-fit: cover;
-  }
-`;
-
-const Hide = styled.div`
-  overflow: hidden;
-`;
 
 export default AboutSection;
