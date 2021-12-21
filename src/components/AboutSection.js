7d1bfe2b8af7f09f60fad7885c68ff8af6c47b28
satriobhirawa <satrio.bhirawa@yahoo.com>
2021-12-21 17:54:30 +0100

    add wave animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import { About, Description, Image, Hide } from "../styles";
 import { motion } from "framer-motion/dist/framer-motion";
 import { titleAnim, fade, photoAnim } from "../animation";
+import Wave from "./Wave";
 
 const AboutSection = () => {
   return (
@@ -31,6 +32,7 @@ const AboutSection = () => {
       <Image>
         <motion.img src={home1} alt="guy with camera" variants={photoAnim} />
       </Image>
+      <Wave />
     </About>
   );
 };
